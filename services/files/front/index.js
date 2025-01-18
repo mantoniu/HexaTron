270e577f2ee9ce357f647a937f2a2aefa950c3c8
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-18 18:05:38 +0100

    refactor(#1): Displacement of the index's javascript

---
+++ services/files/front/index.js
@@ -0,0 +1,7 @@
+import {ModeSelector} from "./components/mode-selector/mode-selector.js"
+import {CustomHeader} from "./components/custom-header/custom-header.js"
+import {CustomNav} from "./components/custom-nav/custom-nav.js"
+
+ModeSelector.register();
+CustomHeader.register();
+CustomNav.register();
