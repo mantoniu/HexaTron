0fa04142b86a72961ddd0592e11c7e3683def177
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-18 14:43:42 +0100

    feat(#1): Addition of a file to load every component

---
+++ services/files/front/components/index-module.js
@@ -0,0 +1,7 @@
+import {ModeSelector} from "./mode-selector/mode-selector.js"
+import {CustomHeader} from "./custom-header/custom-header.js"
+import {CustomNav} from "./custom-nav/custom-nav.js"
+
+ModeSelector.register();
+CustomHeader.register();
+CustomNav.register();
