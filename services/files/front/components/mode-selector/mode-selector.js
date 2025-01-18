e006693d1949878f2936423b71be3fcefaf3fe74
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-18 18:04:56 +0100

    feat(#1): Addition of `CustomButton` in the mode selector

--- services/files/front/components/mode-selector/mode-selector.js
+++ services/files/front/components/mode-selector/mode-selector.js
@@ -1,4 +1,17 @@
-import { Component } from '../component/component.js';
+import {Component} from '../component/component.js';
+import {CustomButton} from "../custom-button/custom-button.js";
 
 export class ModeSelector extends Component {
+    constructor() {
+        super();
+
+        CustomButton.register();
+    }
+
+    async connectedCallback() {
+        await super.connectedCallback();
+
+        this.shadowRoot.getElementById("friendly").addEventListener("click",
+            () => window.location.assign("/components/game-page/game-page.html"));
+    }
 }
