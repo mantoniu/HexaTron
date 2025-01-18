a986e6e89942156764204b26045eb342eb773689
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-18 19:35:37 +0100

    feat(#1): Using `ImageButton` in the nav

--- services/files/front/components/custom-nav/custom-nav.js
+++ services/files/front/components/custom-nav/custom-nav.js
@@ -1,4 +1,10 @@
-import { Component } from '../component/component.js';
+import {Component} from '../component/component.js';
+import {ImageButton} from "../image-button/image-button.js";
 
 export class CustomNav extends Component{
+    constructor() {
+        super();
+
+        ImageButton.register();
+    }
 }
