ae1fa2d34cd80882eb46b2371cf86b7e2afcfe8f
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-01-19 16:17:40 +0100

    feat(#5): Merge main branch and set up board rendering in the canvas within game-board.html

--- services/files/front/components/game-board/game-board.js
+++ services/files/front/components/game-board/game-board.js
@@ -1,4 +1,11 @@
-import {Component} from "../component/component.js";
+import { Component } from "../component/component.js";
+import { GameEngine, GameType } from "../../js/GameEngine.js";
 
 export class GameBoard extends Component {
+    async connectedCallback() {
+        await super.connectedCallback();
+        const canvas = this.shadowRoot.getElementById("board");
+        const context = canvas.getContext("2d");
+        this.gameEngine = new GameEngine(GameType.LOCAL, context);
+    }
 }
