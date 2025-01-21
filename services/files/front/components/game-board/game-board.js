458ae453e3b2bdf3b53a8039e0bc62f9a291fd83
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-21 19:15:32 +0100

    feat(#6): Implementation of the `GameEngine` class to manage a game

--- services/files/front/components/game-board/game-board.js
+++ services/files/front/components/game-board/game-board.js
@@ -1,11 +1,12 @@
-import { Component } from "../component/component.js";
-import { GameEngine, GameType } from "../../js/GameEngine.js";
+import {Component} from "../component/component.js";
+import {GameEngine} from "../../js/GameEngine.js";
 
 export class GameBoard extends Component {
     async connectedCallback() {
         await super.connectedCallback();
         const canvas = this.shadowRoot.getElementById("board");
         const context = canvas.getContext("2d");
-        this.gameEngine = new GameEngine(GameType.LOCAL, context);
+        this.gameEngine = new GameEngine(context, 2, [["a", "q", "e", "d"], ["1", "2", "3", "4"]], ["#ff0000", "#40ff00"]);
+        await this.gameEngine.start();
     }
 }
--- services/files/front/js/GameEngine.js
+++ services/files/front/js/GameEngine.js
@@ -1,25 +1,91 @@
-import { Game } from "./Game.js";
-import { Player } from "./Player.js";
-import { Parameters } from "./Parameters.js";
-
-export const GameType = {
-    LOCAL: 0,
-    IA: 1,
-    RANKED: 2
-};
+import {Game, GameType} from "./Game.js";
+import {Player} from "./Player.js";
+import {Parameters} from "./Parameters.js";
+import {MovementManager} from "./MovementManager.js";
 
 export class GameEngine {
-    constructor(type, context) {
-        this._type = type;
-        let parameters = new Parameters();
-        let player1 = new Player("0", "Player 1", "", parameters);
-        let player2 = new Player("1", "Player 2", "", {});
-        this._game = new Game(9, 16, [player1, player2]);
+    constructor(context, playersCount, keys, colors) {
         this._canvas = context;
-        this._game.board.draw(this._canvas);
+        this._keys = keys;
+        this._colors = colors;
+
+        let players = []
+        for (let i = 0; i < playersCount; i++)
+            players.push(new Player(`${i}`, `Player ${i}`, "", new Parameters()));
+        this._game = new Game(GameType.LOCAL, 9, 16, players, 3);
+
+        this.initializeMovementsManagers();
     }
 
-    fillTile(x, y, idPlayer) {
-        this._game.board.fillTile(x, y, this._game.players[0].parameters._playersColors[0], this._canvas);
+    initialize() {
+        this._game.setPlayersStartPositions();
+        this._game.draw(this._canvas);
+
+        this._movementsManagers.forEach(manager =>
+            manager.initialize(this._game.getPlayerPosition(manager.playerId)));
+
+        for (let i = 0; i < this._game.players.length; i++)
+            this.fillTile(this._game.getPlayerPosition(this._game.players[i].id), this._colors[i]);
+    }
+
+    initializeMovementsManagers() {
+        this._movementsManagers = [];
+
+        for (let i = 0; i < this._game.players.length; i++) {
+            this._movementsManagers.push(
+                new MovementManager(
+                    this._game,
+                    this._canvas,
+                    this._game.players[i].id,
+                    this._colors[i],
+                    this._keys[i],
+                )
+            )
+        }
+    }
+
+    async start() {
+        for (let round = 0; round < this._game.roundsCount; round++) {
+            console.log(`Starting round ${round}`);
+            const result = await this.runRound();
+            this.printResults(result);
+            this._game.resetBoard(this._canvas);
+        }
+        console.log("All rounds completed!");
+    }
+
+    runRound() {
+        return new Promise((resolve) => {
+            this.initialize();
+            let intervalId = setInterval(() => {
+                let results = []
+                this._movementsManagers.forEach(manager => results.push(!manager.handleKeyPress()));
+
+                if (results.includes(true)) {
+                    clearInterval(intervalId);
+                    resolve(results);
+                }
+            }, 200);
+        });
+    }
+
+    printResults(results) {
+        const winners = [];
+        const players = this._game.players;
+
+        for (let i = 0; i < results.length; i++) {
+            if (!results[i])
+                winners.push(players[i].name);
+        }
+
+        if (winners.length === 0)
+            console.log("All players have lost!");
+        else if (winners.length === 1)
+            console.log(`The winner of this round is: ${winners[0]}!`);
+        else console.log(`It's a tie between players: ${winners.join(", ")}!`);
+    }
+
+    fillTile(position, color) {
+        this._game.board.fillTile(position[1], position[0], color, this._canvas);
     }
 }
