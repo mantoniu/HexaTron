de60f648bc9477325dc3872ebefeeb6cd08cb7bf
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-21 19:22:43 +0100

    feat(#6): Addition of Utils for games

---
+++ services/files/front/js/GameUtils.js
@@ -0,0 +1,50 @@
+export const Directions = {
+    UPPER_LEFT: 0,
+    UPPER_RIGHT: 1,
+    RIGHT: 2,
+    LOWER_RIGHT: 3,
+    LOWER_LEFT: 4,
+    LEFT: 5
+};
+
+export function getUpperLeftPosition(position) {
+    return [
+        position[0] - 1,
+        position[0] % 2 === 0 ? position[1] : position[1] - 1
+    ];
+}
+
+export function getUpperRightPosition(position) {
+    return [
+        position[0] - 1,
+        position[0] % 2 === 0 ? position[1] + 1 : position[1]
+    ];
+}
+
+export function getLeftPosition(position) {
+    return [
+        position[0],
+        position[1] - 1
+    ];
+}
+
+export function getRightPosition(position) {
+    return [
+        position[0],
+        position[1] + 1
+    ];
+}
+
+export function getLowerLeftPosition(position) {
+    return [
+        position[0] + 1,
+        position[0] % 2 === 0 ? position[1] : position[1] - 1
+    ];
+}
+
+export function getLowerRightPosition(position) {
+    return [
+        position[0] + 1,
+        position[0] % 2 === 0 ? position[1] + 1 : position[1]
+    ];
+}
