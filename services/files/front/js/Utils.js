3aa48ea64caa4624e039b194fc36b3979b106a44
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-01-21 11:25:35 +0100

    fix(#9): Improve the round counter by filling the circle with the winner's color and adjusting the number's color for better contrast with the circle's fill color

--- services/files/front/js/Utils.js
+++ services/files/front/js/Utils.js
@@ -3,7 +3,6 @@ export function resizeCanvas(context, percentWidth, percentHeight, id, drawingFu
     const canvas = context.shadowRoot.getElementById(id);
     canvas.setAttribute("width", rect.width * percentWidth);
     canvas.setAttribute("height", rect.height * percentHeight);
-    console.log(drawingFunction);
     if (drawingFunction !== null) {
         drawingFunction(callingContext);
     }
@@ -11,4 +10,19 @@ export function resizeCanvas(context, percentWidth, percentHeight, id, drawingFu
 
 export function convertRemToPixels(rem) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
+export function hexToRGB(hex) {
+    let r = parseInt(hex.substring(1, 3), 16);
+    let g = parseInt(hex.substring(3, 5), 16);
+    let b = parseInt(hex.substring(5), 16);
+    return [r, g, b];
+}
+
+export function rgbToHex([r, g, b]) {
+    let red = r.toString(16).padStart(2, "0");
+    let green = g.toString(16).padStart(2, "0");
+    let blue = b.toString(16).padStart(2, "0");
+    return "#" + red + green + blue;
+
 }
