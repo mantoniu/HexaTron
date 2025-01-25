ed1a13218961a666bf761efdddc1cbb8433d91dc
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-25 16:01:59 +0100

    feat(#6): Creating a User class and a mock of it

---
+++ services/files/front/js/User.js
@@ -0,0 +1,28 @@
+export class User {
+    constructor(id, name, profilePicturePath, parameters) {
+        this._id = id;
+        this._name = name;
+        this._profilePicturePath = profilePicturePath;
+        this.parameters = parameters;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get profilePicturePath() {
+        return this._profilePicturePath;
+    }
+
+    get parameters() {
+        return this._parameters;
+    }
+
+    set parameters(parameters) {
+        this._parameters = parameters;
+    }
+}
