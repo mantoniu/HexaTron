d36904cf1c8ffd97641f9b07c655f8ee2340516f
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-25 15:55:18 +0100

    feat(#6): Move a calculation that was unnecessarily repeated in `Board`

--- services/files/front/js/Board.js
+++ services/files/front/js/Board.js
@@ -35,8 +35,10 @@ export class Board {
         }
     }
 
-    isVacant(position) {
-        return this.tiles[position[0]][position[1]].status === Status.VACANT;
+    checkPositionValidity(position) {
+        return this.tiles[position[0]][position[1]].status === Status.VACANT
+            && (position[1] < this.columnCount - 1 && position[1] > 0)
+            && (position[0] < this.rowCount - 1 && position[0] > 0);
     }
 
     calculateUtils(context) {
@@ -50,7 +52,7 @@ export class Board {
         let dy = (3 / 2) * size;
         let gapX = sizeAccordingWidth > sizeAccordingHeight ? (width - dx * (this.tiles[0].length - 2)) / 2 : 0;
         let gapY = sizeAccordingHeight > sizeAccordingWidth ? (height - 3 * size * Math.floor((this.tiles.length - 2) / 2) - (this.tiles.length % 2 === 1 ? 2 * size : 0)) / 2 : 0;
-        return [dx, dy, gapX, gapY, size];
+        return {dx, dy, gapX, gapY, size};
     }
 
     drawLine(i, j, dx, dy, gapX, gapY, size, context) {
@@ -63,21 +65,30 @@ export class Board {
     draw(context) {
         context.strokeStyle = "black";
         context.lineWidth = 1;
-        let [dx, dy, gapX, gapY, size] = this.calculateUtils(context);
+        this._utils = this.calculateUtils(context);
+
         for (let i = 1; i < this.tiles.length - 1; i++) {
             if (i % 2 === 1) {
                 for (let j = 1; j < this.tiles[i].length - 1; j++) {
-                    this.tiles[i][j].draw(gapX + dx * (j - 0.5), gapY + dy * (i - 1), size, context, j === 1);
+                    this.tiles[i][j].draw(this._utils.gapX + this._utils.dx * (j - 0.5), this._utils.gapY + this._utils.dy * (i - 1), this._utils.size, context, j === 1);
                     if (i !== 0 && i !== this.tiles.length - 2)
-                        this.drawLine(i, j, dx, dy, gapX, gapY, size, context);
+                        this.drawLine(i, j, this._utils.dx, this._utils.dy, this._utils.gapX, this._utils.gapY, this._utils.size, context);
                 }
             }
         }
     }
 
-    fillTile(i, j, color, context) {
-        let [dx, dy, gapX, gapY, size] = this.calculateUtils(context);
-        this.tiles[i][j].fillTile(gapX + dx * (j - (i % 2 === 1 ? 0.5 : 0)), gapY + dy * (i - 1), size, context, color);
-        this.tiles[i][j].status = Status.TAKEN;
+    fillTile(pos, color, direction, context, drawBike) {
+        let dPos = [this._utils.gapX + this._utils.dx * (pos[1] - (pos[0] % 2 === 1 ? 0.5 : 0)), this._utils.gapY + this._utils.dy * (pos[0] - 1)];
+        this.tiles[pos[0]][pos[1]].fill(dPos, this._utils.size, context, color, direction, drawBike)
+    }
+
+    update(previousPos, nextPos, color, context, direction) {
+        this.fillTile(nextPos, color, direction, context, true);
+
+        if (previousPos)
+            this.fillTile(previousPos, color, direction, context, false);
+
+        this.tiles[nextPos[0]][nextPos[1]].status = Status.TAKEN;
     }
 }
