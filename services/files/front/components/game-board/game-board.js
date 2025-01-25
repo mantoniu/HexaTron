8a117b9d5f176096ea5b8abde25f225bb11fad68
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-25 15:57:22 +0100

    feat(#6): General improvements to the `GameEngine` class, the game loop and the way results are taken into account

--- services/files/front/components/game-board/game-board.js
+++ services/files/front/components/game-board/game-board.js
@@ -1,12 +1,14 @@
 import {Component} from "../component/component.js";
 import {GameEngine} from "../../js/GameEngine.js";
+import {CURRENT_USER} from "../../js/UserMock.js";
+import {GameType} from "../../js/Game.js";
 
 export class GameBoard extends Component {
     async connectedCallback() {
         await super.connectedCallback();
         const canvas = this.shadowRoot.getElementById("board");
         const context = canvas.getContext("2d");
-        this.gameEngine = new GameEngine(context, 2, [["a", "q", "e", "d"], ["1", "2", "3", "4"]], ["#ff0000", "#40ff00"]);
+        this.gameEngine = new GameEngine([CURRENT_USER], GameType.LOCAL, 9, 16, 3, 2, context, 200);
         await this.gameEngine.start();
     }
 }
