e73ed9cbbd312b181e8f66c40df7a96468fe4628
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-01-26 16:51:10 +0100

    feat(#13): Try to store the socket to make it persistent

--- services/files/front/components/game-page/game-page.js
+++ services/files/front/components/game-page/game-page.js
@@ -2,8 +2,10 @@ import { CustomNav } from "../custom-nav/custom-nav.js";
 import { GameBoard } from "../game-board/game-board.js";
 import { HomeButton } from "../home-button/home-button.js";
 import { GameHeader } from "../game-header/game-header.js";
+import { wsService } from "../../js/Socket.js";
 
 function onLoad() {
+    console.log(wsService.socket);
     document.addEventListener("game-created", (event) => {
         const gameHeader = document.querySelector("game-header");
         if (gameHeader)
--- services/files/front/index.js
+++ services/files/front/index.js
@@ -1,19 +1,11 @@
 import { ModeSelector } from "./components/mode-selector/mode-selector.js";
 import { CustomNav } from "./components/custom-nav/custom-nav.js";
 import { HomeButton } from "./components/home-button/home-button.js";
+import { wsService } from "./js/Socket.js";
 
 function loaded() {
-    const socket = io(`http://${window.location.hostname}:8000/`, {
-        path: "/api/socket.io/"
-    });
-
-    socket.on("connect", () => {
-        console.log("Connected to server with ID:", socket.id);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("Disconnected from server");
-    });
+    if (wsService.socket === null)
+        wsService.socket = "";
 }
 
 ModeSelector.register();
