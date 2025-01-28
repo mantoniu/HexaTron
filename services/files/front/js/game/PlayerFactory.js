88b94d77c8ca97ef780edfe3ec3aae94ff26ed04
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-01-28 15:20:39 +0100

    fix(#20): AI unsupported type

--- services/files/front/js/game/PlayerFactory.js
+++ services/files/front/js/game/PlayerFactory.js
@@ -1,14 +1,11 @@
 import {GameType} from "./Game.js";
 import {LocalPlayer} from "./LocalPlayer.js";
-import {RandomAI} from "../ai/RandomAI.js";
 
 export class PlayerFactory {
     static createPlayer(type, id, color, keys = null) {
         switch (type) {
             case GameType.LOCAL:
                 return new LocalPlayer(id, "Guest " + id, color, null, keys);
-            case GameType.AI:
-                return new RandomAI(id, "AI " + id, color);
             default:
                 throw new Error(`Unsupported player type: ${type}`);
         }
