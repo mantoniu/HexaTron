32fec8c6a09cb566431ba8ac98ce9c826191e627
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-02-08 10:40:25 +0100

    feat(#13): Updating the game's classes to keep just the front end

--- services/files/front/js/game/Game.js
+++ services/files/front/js/game/Game.js
@@ -1,6 +1,8 @@
 import {Board} from "./Board.js";
 import {getRandomInt} from "../Utils.js";
 import {Position} from "./Position.js";
+import {Directions, DISPLACEMENT_FUNCTIONS} from "./GameUtils.js";
+import {CURRENT_USER} from "../UserMock.js";
 
 export const GameType = {
     LOCAL: 0,
@@ -9,12 +11,22 @@ export const GameType = {
 };
 
 export class Game {
-    constructor(type, rowNumber, columnNumber, players, roundsCount) {
+    constructor(type, rowNumber, columnNumber, players, roundsCount, context) {
+        this._id = null;
+        this._context = context;
         this._type = type;
         this._board = new Board(rowNumber, columnNumber);
         this._players = players;
         this._playersPositions = {};
         this._roundsCount = roundsCount;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(newId) {
+        this._id = newId;
     }
 
     get players() {
@@ -41,25 +53,58 @@ export class Game {
         this._playersPositions = playersPosition;
     }
 
+    obtainDirection(prevPosition, newPosition) {
+        if (!prevPosition || prevPosition.equals(newPosition))
+            return newPosition.column === 1
+                ? Directions.RIGHT
+                : Directions.LEFT;
+
+        const displacementResults = DISPLACEMENT_FUNCTIONS.map((fun, index) => ({
+            result: fun(prevPosition),
+            index
+        }));
+
+        return displacementResults.find(({result}) => result.equals(newPosition)).index;
+    }
+
+    refreshBoard(playersPosition) {
+        Object.entries(playersPosition).forEach(([player, position], i) => {
+            const curPosition = new Position(...Object.values(position));
+            const prevPosition = this._playersPositions[player];
+
+            this.board.update(
+                prevPosition,
+                curPosition,
+                CURRENT_USER.parameters.playersColors[i],
+                this._context,
+                this.obtainDirection(prevPosition, curPosition)
+            );
+
+            this._playersPositions[player] = curPosition;
+        });
+    }
+
     getPlayerPosition(playerId) {
         return this._playersPositions[playerId];
     }
 
     setPlayerPosition(playerId, position) {
         this._playersPositions[playerId] = position;
+        this.board.fillTile(position, "black", Directions.UPPER_RIGHT, this._context, false);
     }
 
     get board() {
         return this._board;
     }
 
-    resetBoard(canvas) {
+    resetBoard() {
         this._board.initialize();
-        canvas.reset();
+        this._context.reset();
+        this.draw();
     }
 
-    draw(canvas) {
-        this.board.draw(canvas);
+    draw() {
+        this.board.draw(this._context);
     }
 
     generatePossibleStartPositions() {
@@ -91,5 +136,30 @@ export class Game {
             resultDict[this.players[i].id] = possiblesPositions[i];
 
         this.playersPositions = resultDict;
+    }
+
+    printRoundEndResults(name) {
+        console.log(`The winner of this round is: ${name}!`);
+    }
+
+    printTiesResults(ties) {
+        const message = ties
+            .map(tie => "There is a tie between players: " + [...tie].join(", "))
+            .join("\n");
+
+        console.log(message);
+    }
+
+    printResults(result) {
+        switch (result.status) {
+            case "tie":
+                this.printTiesResults(result.ties);
+                break;
+            case "winner":
+                this.printRoundEndResults(result.winner);
+                break;
+            default:
+                throw Error(`The status ${result.status} is not yet supported`);
+        }
     }
 }
