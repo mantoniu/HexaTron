9d438be9d74efdd05e191c4d62e3fca2743c3fe0
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-02-08 15:42:03 +0100

    feat(#22): Refactor the two bots by putting all common parts into the AI class and AI utils.

---
+++ services/files/front/js/ai/AIUtils.js
@@ -0,0 +1,54 @@
+function getUpperLeftPosition(position) {
+    return [
+        position[0] - 1,
+        position[0] % 2 === 0 ? position[1] : position[1] - 1
+    ];
+}
+
+function getUpperRightPosition(position) {
+    return [
+        position[0] - 1,
+        position[0] % 2 === 0 ? position[1] + 1 : position[1]
+    ];
+}
+
+function getLeftPosition(position) {
+    return [
+        position[0],
+        position[1] - 1
+    ];
+}
+
+function getRightPosition(position) {
+    return [
+        position[0],
+        position[1] + 1
+    ];
+}
+
+function getLowerLeftPosition(position) {
+    return [
+        position[0] + 1,
+        position[0] % 2 === 0 ? position[1] : position[1] - 1
+    ];
+}
+
+function getLowerRightPosition(position) {
+    return [
+        position[0] + 1,
+        position[0] % 2 === 0 ? position[1] + 1 : position[1]
+    ];
+}
+
+export const DISPLACEMENT_FUNCTIONS = [
+    getUpperLeftPosition,
+    getUpperRightPosition,
+    getRightPosition,
+    getLowerRightPosition,
+    getLowerLeftPosition,
+    getLeftPosition
+];
+
+export function neighbour(position, side) {
+    return DISPLACEMENT_FUNCTIONS[side](position);
+}
