844e266bd91e6188b779723b09ddefcb86be5c22
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-02-09 00:34:18 +0100

    refactor(#13): Removing two useless registers

--- services/files/front/components/game-component/game-component.js
+++ services/files/front/components/game-component/game-component.js
@@ -1,6 +1,4 @@
-import {CustomNav} from "../custom-nav/custom-nav.js";
 import {GameBoard} from "../game-board/game-board.js";
-import {HomeButton} from "../home-button/home-button.js";
 import {GameHeader} from "../game-header/game-header.js";
 import {Component} from "../component/component.js";
 
@@ -8,9 +6,7 @@ export class GameComponent extends Component {
     constructor() {
         super();
 
-        HomeButton.register();
         GameBoard.register();
         GameHeader.register();
-        CustomNav.register();
     }
 }
--- services/files/front/components/mode-selector/mode-selector.js
+++ services/files/front/components/mode-selector/mode-selector.js
@@ -19,15 +19,38 @@ export class ModeSelector extends Component {
     async connectedCallback() {
         await super.connectedCallback();
 
-        setTimeout(() => {
-            this.shadowRoot.querySelectorAll("custom-button").forEach(button => {
-                button.addEventListener("click", () => {
-                    const gameType = Number(button.getAttribute("game-type"));
+        this._gameCreatedHandler = () => this._handleGameCreated();
 
-                    GameService.getInstance().startGame(gameType, 9, 16, 3, 2);
-                });
-            });
-        }, 0);
+        GameService.getInstance().on(
+            GameStatus.CREATED,
+            this._gameCreatedHandler
+        );
+
+        const buttons = this.shadowRoot.querySelectorAll("custom-button");
+        buttons.forEach(button => {
+            const handler = () => {
+                const gameType = Number(button.getAttribute("game-type"));
+                GameService.getInstance().startGame(gameType, 9, 16, 3, 2);
+            };
+
             this.addAutoCleanListener(button, 'click', handler);
+        });
+    }
+
+    disconnectedCallback() {
+        super.disconnectedCallback();
+
+        if (this._gameCreatedHandler) {
+            GameService.getInstance().off(
+                GameStatus.CREATED,
+                this._gameCreatedHandler
+            );
+        }
+    }
+
+    _handleGameCreated() {
+        window.dispatchEvent(new CustomEvent("navigate", {
+            detail: {route: "/game"}
+        }));
     }
 }
