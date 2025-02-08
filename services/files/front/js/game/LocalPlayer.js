942404403adaec4fede3c38667ab6d927cb877d7
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-02-08 20:57:17 +0100

    refactor(#13): Deleting duplicate fields with users in players

--- services/files/front/js/game/LocalPlayer.js
+++ services/files/front/js/game/LocalPlayer.js
@@ -3,8 +3,8 @@ import {defaultMovementsConfiguration} from "./GameUtils.js";
 import {GameService} from "../../services/game-service.js";
 
 export class LocalPlayer extends Player {
-    constructor(id, name, color, profilePicturePath, keyConfiguration) {
-        super(id, name, color, profilePicturePath);
+    constructor(id, name, keyConfiguration) {
+        super(id, name);
         this._keys = keyConfiguration;
         this._movementsMapping = defaultMovementsConfiguration(keyConfiguration);
         this._currentResolve = null;
@@ -16,7 +16,7 @@ export class LocalPlayer extends Player {
         window.addEventListener('keydown', (event) => {
             const key = event.key.toLowerCase();
 
-            if (this._keys.includes(key) && GameService.getInstance().game) {
+            if (this._keys.includes(key) && GameService.getInstance().game.id) {
                 const move = this._movementsMapping[key];
                 GameService.getInstance().nextMove(this.id, move);
             }
