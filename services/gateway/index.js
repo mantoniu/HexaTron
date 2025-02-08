947d3d5c798ebcd861f5e113e9c2b5321e9c604b
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-02-08 10:37:44 +0100

    feat(#13): Added management of socket redirections in the gateway

--- services/gateway/index.js
+++ services/gateway/index.js
@@ -1,6 +1,8 @@
 // The http module contains methods to handle http queries.
 const http = require('http');
 const httpProxy = require('http-proxy');
+const {Server} = require('socket.io');
+const {io: Client} = require('socket.io-client');
 
 // We will need a proxy to send requests to the other services.
 const proxy = httpProxy.createProxyServer();
@@ -19,8 +21,6 @@ const server = http.createServer(function (request, response) {
         //console.log(request.headers)
         // If the URL starts by /api, then it's a REST request (you can change that if you want).
         if (filePath[1] === "api") {
-            console.log("API");
-            proxy.web(request, response, { target: "http://127.0.0.1:8002" });
             //TODO: Add middlewares and call microservices depending on the request.
 
         // If it doesn't start by /api, then it's a request for a file.
@@ -36,17 +36,54 @@ const server = http.createServer(function (request, response) {
 // For the server to be listening to request, it needs a port, which is set thanks to the listen function.
 })
 
-// You also need to handle the upgrade requests for WebSockets.
-server.on("upgrade", (request, socket, head) => {
-    // Check if the URL corresponds to a WebSocket (here, "/api/socket.io")
-    if (request.url.startsWith("/api/socket.io")) {
-        console.log("WebSocket connection upgrade");
-        // If it matches, redirect this connection to the WebSocket server.
-        proxy.ws(request, socket, head, { target: "ws://127.0.0.1:8002" });
-    } else {
-        socket.write("HTTP/1.1 400 Bad Request\r\n");
-        socket.end();
+const ioServer = new Server(server, {
+    cors: {
+        origin: ["http://localhost:8002", "http://localhost:8001"],
+        methods: ["GET", "POST"]
     }
 });
 
-server.listen(8000);
+const gameServiceSocket = Client('http://localhost:8002');
+const gameNamespace = ioServer.of("/game");
+
+const servicesNamespaces = {
+    game: {
+        namespace: gameNamespace,
+        socket: gameServiceSocket,
+    }
+};
+
+Object.entries(servicesNamespaces).forEach(([serviceName, service]) => {
+    const {namespace, socket} = service;
+
+    namespace.on("connection", (clientSocket) => {
+        clientSocket.onAny((eventName, ...args) => {
+            socket.emit(eventName, {
+                socketId: clientSocket.id,
+                args
+            });
+        });
+
+        clientSocket.on("disconnect", () => {
+            socket.emit(`leave${serviceName}`, clientSocket.id);
+        });
+    });
+
+    socket.onAny((eventName, data) => {
+        if (data.room) {
+            namespace.to(data.room).emit(eventName, data.eventData);
+        }
+    });
+
+    socket.on("connect", () => {
+        console.log(`✅ Gateway connected to ${serviceName} Service`);
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`❌ Gateway disconnected from ${serviceName} Service`);
+    });
+});
+
+server.listen(8000, () => {
+    console.log("🌐 Gateway listening on http://localhost:8000");
+});
