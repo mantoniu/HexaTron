fc1fa3a864c1d26254819e01fb7dce198a0dfa68
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-02-09 19:12:09 +0100

    feat(#26): Create a user service separate from the database service; the latter will be used only to initialize the database.

--- services/database/index.js
+++ services/database/index.js
@@ -4,16 +4,19 @@ const {User} = require("./TypesValidation");
 const uri = "mongodb://root:root@localhost:27017/?authSource=admin";
 const client = new MongoClient(uri);
 const dbName = "database";
+const db = client.db(dbName);
 const collectionNamesList = ["users"];
 const propertiesCollections = {
     users: User
+};
+const collectionsIndex = {
+    users: {name: 1}
 };
 
 async function startService() {
     try {
         await client.connect();
 
-        const db = client.db(dbName);
         const collections = await db.listCollections({}, {nameOnly: true}).toArray();
 
         for (const collectionName of collectionNamesList) {
@@ -29,48 +32,21 @@ async function startService() {
                             properties: propertiesCollections[collectionName]
                         }
                     },
-                    validationAction: "error" // Rejeter les documents qui ne respectent pas la validation
+                    validationAction: "error"
                 });
+                await db.collection(collectionName).createIndex(collectionsIndex[collectionName], {unique: true});
                 console.log("Collection '${collectionName}' created with validation.");
             } else {
                 console.log("The collection '${collectionName}' already exists.");
             }
         }
 
-    } catch (err) {
-        console.error("Connection error: ", err);
+    } catch (error) {
+        console.error("Error during initialization: ", error);
+    } finally {
+        await client.close();
+        console.log("MongoDB connection closed");
     }
 }
 
-
-async function createUser(data) {
-    const db = client.db(dbName);
-
-
-    const result = await db.collection("users").insertOne(data);
-
-    const users2 = await db.collection("users").find().toArray();
-    console.log(users2, result);
-}
-
-
-async function main() {
-    const db = client.db(dbName);
-
-    for (const collectionName of collectionNamesList) {
-        const collections = await db.listCollections({name: collectionName}).toArray();
-        await db.dropCollection(collectionName);
-    }
-    await startService();
-    await createUser({name: "Jilian", email: "test@test.com", age: 20});
-}
-
-
-main();
-
-
-process.on("SIGINT", async () => {
-    await client.close();
-    console.log("MongoDB connection closed");
-    process.exit(0);
-});
+startService().then(() => process.exit(0));
