e3af036b4305df1ee54ec382422ef23d728f7ca5
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-02-10 21:42:46 +0100

    fix(#27): Refresh when submitting on firefox

--- services/files/front/components/submit-button/submit-button.js
+++ services/files/front/components/submit-button/submit-button.js
@@ -1,10 +1,4 @@
 import {Component} from "../component/component.js";
 
 export class SubmitButton extends Component {
-    async connectedCallback() {
-        await super.connectedCallback();
-
-        this.addAutoCleanListener(this.shadowRoot.getElementById("submit-button"), "click",
-            () => this.closest("form").dispatchEvent(new Event("submit")));
-    }
 }
