cc675d3de23eadf97be962101f7acf7dca1fd123
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-02-10 20:16:36 +0100

    feat(#27): Implementation of a login portal

---
+++ services/files/front/components/login-portal/login-portal.js
@@ -0,0 +1,36 @@
+import {SubmitButton} from "../submit-button/submit-button.js";
+import {UserService} from "../../services/user-service.js";
+import {BaseAuth} from "../base-auth/base-auth.js";
+
+export class LoginPortal extends BaseAuth {
+    constructor() {
+        super();
+
+        SubmitButton.register();
+    }
+
+    async connectedCallback() {
+        await super.connectedCallback();
+
+
+        const actionLinks = this.shadowRoot.querySelectorAll("[data-action]");
+        actionLinks.forEach(link => {
+            this.addAutoCleanListener(link, "click", (event) => this.handleActionClick(event));
+        });
+    }
+
+    handleActionClick(event) {
+        const action = event.target.getAttribute("data-action");
+        if (action) {
+            this.dispatchEvent(new CustomEvent("changeContent", {
+                bubbles: true,
+                composed: true,
+                detail: action,
+            }));
+        }
+    }
+
+    handleSubmit() {
+        UserService.getInstance().login(this.getFormData());
+    }
+}
