af21c3cd0ad5ac957e34988783e3b33b253e962a
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-02-10 19:58:36 +0100

    feat(#26): Create all the API calls in the user service and update the database service

--- services/database/index.js
+++ services/database/index.js
@@ -1,29 +1,32 @@
 const {MongoClient} = require("mongodb");
-const {User} = require("./TypesValidation");
+const {User, RefreshToken} = require("./TypesValidation");
 
 const uri = "mongodb://root:root@localhost:27017/?authSource=admin";
 const client = new MongoClient(uri);
 const dbName = "database";
 const db = client.db(dbName);
-const collectionNamesList = ["users"];
+const collectionNamesList = ["users", "refreshTokens"];
 const propertiesCollections = {
-    users: User
+    users: User,
+    refreshTokens: RefreshToken
 };
 const collectionsIndex = {
-    users: {name: 1}
+    users: {name: 1},
+    refreshTokens: {userID: 1, refreshToken: 1}
 };
 
 async function startService() {
     try {
         await client.connect();
 
-        const collections = await db.listCollections({}, {nameOnly: true}).toArray();
 
         for (const collectionName of collectionNamesList) {
-
-            const collectionExists = collections.some(collection => collection.name === collectionName);
-
-            if (collectionExists) {
+            await db.dropCollection(collectionName);
+            const collections = await db.listCollections({}, {nameOnly: true}).toArray();
+            const collectionExists = collections.some(collection => {
+                return collection.name === collectionName;
+            });
+            if (!collectionExists) {
                 await db.createCollection(collectionName, {
                     validator: {
                         $jsonSchema: {
@@ -35,9 +38,9 @@ async function startService() {
                     validationAction: "error"
                 });
                 await db.collection(collectionName).createIndex(collectionsIndex[collectionName], {unique: true});
-                console.log("Collection '${collectionName}' created with validation.");
+                console.log(`Collection '${collectionName}' created with validation.`);
             } else {
-                console.log("The collection '${collectionName}' already exists.");
+                console.log(`The collection '${collectionName}' already exists.`);
             }
         }
 
