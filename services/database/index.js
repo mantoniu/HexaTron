569a0bb7ebf41bb1070f6d7fac3781c53439f9c6
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-02-17 17:37:31 +0100

    feature(#34): Add users and roles to the database for each service to prevent unintended modifications to collections not managed by the service.

--- services/database/index.js
+++ services/database/index.js
@@ -1,11 +1,10 @@
 const {MongoClient} = require("mongodb");
 const {User, RefreshToken} = require("./TypesValidation.js");
-
 const uri = process.env.URI;
 const client = new MongoClient(uri);
-const dbName = "database";
+const dbName = process.env.DB_NAME;
 const db = client.db(dbName);
-const collectionNamesList = ["users", "refreshTokens"];
+const adminDb = client.db("admin");
 const propertiesCollections = {
     users: User,
     refreshTokens: RefreshToken
@@ -14,16 +13,59 @@ const collectionsIndex = {
     users: {name: 1},
     refreshTokens: {userID: 1, refreshToken: 1}
 };
+const collections = process.env.COLLECTIONS.split(",").map(collection => collection.trim());
+const users = process.env.USERS.split(",").map(user => {
+    user = user.split(";");
+    return {"userName": user[0], "password": user[1]};
+});
+const users_collections = {
+    [collections[0]]: users[0],
+    [collections[1]]: users[0]
+};
 
 async function startService() {
     try {
         await client.connect();
 
+        for (const collectionName of Object.keys(users_collections)) {
+            const user = await adminDb.command({usersInfo: users_collections[collectionName]["userName"]});
+            if (!(user.users && user.users.length > 0)) {
+                await adminDb.command({
+                    createUser: users_collections[collectionName]["userName"],
+                    pwd: users_collections[collectionName]["password"],
+                    roles: []
+                });
+                console.log(`The user "${users_collections[collectionName]["userName"]}" has been successfully created.`);
+            }
 
-        for (const collectionName of collectionNamesList) {
-            await db.dropCollection(collectionName);
-            const collections = await db.listCollections({}, {nameOnly: true}).toArray();
-            const collectionExists = collections.some(collection => {
+            const existingRole = await adminDb.command({rolesInfo: users_collections[collectionName]["userName"]});
+            if (!existingRole.roles.length > 0) {
+                await adminDb.command({
+                    createRole: users_collections[collectionName]["userName"],
+                    privileges: [
+                        {resource: {db: dbName, collection: collectionName}, actions: ["find", "insert", "update", "remove"]}
+                    ],
+                    roles: []
+                });
+                console.log(`The role "${users_collections[collectionName]["userName"]}" has been successfully created.`);
+            }
+
+            await adminDb.command({
+                grantPrivilegesToRole: users_collections[collectionName]["userName"],
+                privileges: [
+                    {resource: {db: dbName, collection: collectionName}, actions: ["find", "insert", "update", "remove"]}
+                ]
+            });
+
+            await adminDb.command({
+                grantRolesToUser: users_collections[collectionName]["userName"],
+                roles: [
+                    {role: users_collections[collectionName]["userName"], db: "admin"}
+                ]
+            });
+
+            const existentCollections = await db.listCollections({}, {nameOnly: true}).toArray();
+            const collectionExists = existentCollections.some(collection => {
                 return collection.name === collectionName;
             });
             if (!collectionExists) {
