d85de89e416a10673d78ac199c9be26720464e93
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-02-19 18:41:31 +0100

    feat(#32): Implementation of the forgotten password portal

--- services/files/front/components/forgotten-password-portal/forgotten-password-portal.js
+++ services/files/front/components/forgotten-password-portal/forgotten-password-portal.js
@@ -2,7 +2,37 @@ import {BaseAuth} from "../base-auth/base-auth.js";
 import {UserService} from "../../services/user-service.js";
 
 export class ForgottenPasswordPortal extends BaseAuth {
+    async connectedCallback() {
+        await super.connectedCallback();
+
+        this._resetPasswordHandler = (data) => {
+            if (data.success) {
+                this.dispatchEvent(new CustomEvent("changeContent", {
+                    bubbles: true,
+                    composed: true,
+                    detail: "profile",
+                }));
+            } else alert(data.error);
+        };
+
+        UserService.getInstance().on("resetPassword", this._resetPasswordHandler);
+    }
+
     handleSubmit() {
-        UserService.getInstance().resetPassword(this.getFormData());
+        const data = this.getFormData();
+        const username = data["username"];
+        const password = data["password"];
+        const answers = [
+            data["security-question1"],
+            data["security-question2"],
+            data["security-question3"]
+        ];
+
+        UserService.getInstance().resetPassword({username, password, answers});
+    }
+
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        UserService.getInstance().off("resetPassword", this._resetPasswordHandler);
     }
 }
