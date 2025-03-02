886bb4b6782eceb0e5349151da36c13c393f20c5
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-03-02 12:12:02 +0100

    feat(#43): Game reversal support added to `GameService` front-end

--- services/files/front/services/game-service.js
+++ services/files/front/services/game-service.js
@@ -1,6 +1,7 @@
 import {Game, GameType} from "../js/game/Game.js";
 import {LocalPlayer} from "../js/game/LocalPlayer.js";
-import {CURRENT_USER} from "../js/UserMock.js";
+import {UserService} from "./user-service.js";
+import {MovementTypes} from "../js/game/GameUtils.js";
 
 export const GameStatus = {
     CREATED: 'CREATED',
@@ -19,6 +20,7 @@ export class GameService {
         this._game = null;
         this._context = null;
         this._socket = io(`http://${window.location.hostname}/game`);
+        this._shouldInvertPositions = false;
 
         this.socket.on("connect", () => {
             console.log("user connected " + this._socket.id);
@@ -106,7 +108,7 @@ export class GameService {
                     this.handleGameCreated(data);
                     break;
                 case GameStatus.POSITIONS_UPDATED:
-                    this.handlePositionsUpdated(data);
+                    this.handlePositionsUpdated(data.newPositions);
                     break;
                 case GameStatus.ROUND_END:
                     this.handleRoundEnd(data);
@@ -125,11 +127,34 @@ export class GameService {
     handleGameCreated(data) {
         this.emit(GameStatus.CREATED);
         this.game.id = data.id;
-        this.game.players = data.players;
+
+        this._shouldInvertPositions = Object.keys(data.players)[0] !== UserService.getInstance().user._id;
+        this.game.players = this._shouldInvertPositions
+            ? Object.fromEntries(Object.entries(data.players).reverse())
+            : data.players;
     }
 
-    handlePositionsUpdated(data) {
-        this.game.refreshBoard(data.newPositions, this._context);
+    _invertPosition(position) {
+        return {
+            _row: position._row,
+            _column: position._row % 2 === 0 ? this.game.board.columnCount - position._column - 2 : this.game.board.columnCount - position._column - 1,
+        };
+    }
+
+    handlePositionsUpdated(newPositions) {
+        if (!this._shouldInvertPositions) {
+            this.game.refreshBoard(newPositions, this._context);
+            return;
+        }
+
+        const updatedPositions = Object.fromEntries(
+            Object.entries(newPositions).map(([playerId, position]) => [
+                playerId,
+                this._invertPosition(position)
+            ])
+        );
+
+        this.game.refreshBoard(updatedPositions, this._context);
     }
 
     handleRoundEnd(data) {
@@ -148,28 +173,25 @@ export class GameService {
         window.location.href = "/";
         this._game = null;
         this._context = null;
+        this._shouldInvertPositions = false;
     }
 
     startGame(gameType, rowNumber, columnNumber, roundsCount, playersCount, context) {
         if (this.game?.id)
             return;
 
-        const user = new LocalPlayer(CURRENT_USER.id, CURRENT_USER.name, CURRENT_USER.parameters.keys[0]);
+        const user = new LocalPlayer(UserService.getInstance().user._id, UserService.getInstance().user.name, UserService.getInstance().user.parameters.keysPlayers[0]);
 
         const users = [user];
         if (gameType === GameType.LOCAL) {
-            const guest = new LocalPlayer("1", "Guest", CURRENT_USER.parameters.keys[1]);
+            const guest = new LocalPlayer("guest", "Guest", UserService.getInstance().user.parameters.keysPlayers[1]);
             users.push(guest);
         }
 
         this._game = new Game(gameType, rowNumber, columnNumber, users, roundsCount, context);
 
-        this.socket.emit("start", {
+        this.socket.emit("joinGame", {
             gameType,
-            rowNumber,
-            columnNumber,
-            roundsCount,
-            playersCount,
             users: users.map(user => ({id: user.id, name: user.name}))
         });
     }
@@ -179,7 +201,20 @@ export class GameService {
             this.game.draw(this._context);
     }
 
+    _invertMove(move) {
+        const movements = Object.values(MovementTypes);
+        const index = movements.indexOf(move);
+        if (index === -1)
+            return move;
+
+        const mirroredIndex = movements.length - 1 - index;
+        return movements[mirroredIndex];
+    }
+
     nextMove(playerId, move) {
+        if (this._shouldInvertPositions)
+            move = this._invertMove(move);
+
         this.socket.emit("nextMove", {gameId: this.game.id, playerId, move});
     }
 }
