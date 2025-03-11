4059ce5d7c3e04bbd522f67c436b87fc9284b1b5
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-03-11 14:52:52 +0100

    feat(#65): Creation of a `SocketService` to add the chat service

---
+++ services/files/front/services/socket-service.js
@@ -0,0 +1,89 @@
+/**
+ * Service for managing Socket.IO connections.
+ *
+ * @class
+ */
+class SocketService {
+    /**
+     * Singleton instance of SocketService.
+     *
+     * @static
+     * @type {SocketService}
+     */
+    static _instance = null;
+
+    /**
+     * Sockets managed by this service.
+     *
+     * @type {Object}
+     * @property {Socket} game - Socket for the `/game` namespace.
+     * @property {Socket} chat - Socket for the `/chat` namespace.
+     */
+    _sockets = {
+        game: null,
+        chat: null,
+    };
+
+    /**
+     * Private constructor (singleton pattern).
+     *
+     * @private
+     */
+    constructor() {
+        if (SocketService._instance)
+            return SocketService._instance;
+
+        this._sockets.game = io(`http://${window.location.hostname}/game`, {autoConnect: true});
+        this._sockets.chat = io(`http://${window.location.hostname}/chat`, {autoConnect: true});
+
+        this.setupListeners();
+
+        SocketService._instance = this;
+    }
+
+    /**
+     * Returns the socket for the `/game` namespace.
+     *
+     * @returns {Socket} The game socket.
+     */
+    get gameSocket() {
+        return this._sockets.game;
+    }
+
+    /**
+     * Returns the socket for the `/chat` namespace.
+     *
+     * @returns {Socket} The chat socket.
+     */
+    get chatSocket() {
+        return this._sockets.chat;
+    }
+
+    /**
+     * Returns the singleton instance of SocketService.
+     *
+     * @static
+     * @returns {SocketService} The singleton instance.
+     */
+    static getInstance() {
+        if (!SocketService._instance)
+            SocketService._instance = new SocketService();
+
+        return SocketService._instance;
+    }
+
+    /**
+     * Sets up event listeners for the sockets.
+     *
+     * @private
+     */
+    setupListeners() {
+        this._sockets.game.on('connect', () => console.log('[GameSocket] Connected:', this._sockets.game.id));
+        this._sockets.game.on('disconnect', () => console.warn('[GameSocket] Disconnected!'));
+
+        this._sockets.chat.on('connect', () => console.log('[ChatSocket] Connected:', this._sockets.chat.id));
+        this._sockets.chat.on('disconnect', () => console.warn('[ChatSocket] Disconnected!'));
+    }
+}
+
+export const socketService = SocketService.getInstance();
