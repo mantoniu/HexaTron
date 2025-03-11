294156906bb92d8cfe23cd2acf617555a234c05b
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-03-11 14:53:26 +0100

    docs(#65): Addition of comments in the `GameService`

--- services/files/front/services/game-service.js
+++ services/files/front/services/game-service.js
@@ -1,9 +1,15 @@
 import {Game, GameType} from "../js/game/Game.js";
 import {LocalPlayer} from "../js/game/LocalPlayer.js";
-import {UserService} from "./user-service.js";
+import {userService} from "./user-service.js";
 import {MovementTypes} from "../js/game/GameUtils.js";
 import {EventEmitter} from "../js/EventEmitter.js";
+import {socketService} from "./socket-service.js";
 
+/**
+ * Enum representing the possible statuses of a game.
+ *
+ * @constant {Object}
+ */
 export const GameStatus = {
     CREATED: 'CREATED',
     POSITIONS_UPDATED: 'POSITION_UPDATED',
@@ -11,9 +17,22 @@ export const GameStatus = {
     GAME_END: 'GAME_END'
 };
 
-export class GameService extends EventEmitter {
+/**
+ * Service for game management.
+ * This class implements the Singleton pattern to ensure a single instance.
+ * It extends EventEmitter to handle game-related events.
+ *
+ * @class GameService
+ * @extends EventEmitter
+ * @singleton
+ */
+class GameService extends EventEmitter {
     static _instance = null;
 
+    /**
+     * Creates an instance of GameService or returns the existing instance.
+     * Initializes the game state, socket connection, and event listeners.
+     */
     constructor() {
         super();
 
@@ -22,7 +41,7 @@ export class GameService extends EventEmitter {
 
         this._game = null;
         this._context = null;
-        this._socket = io(`http://${window.location.hostname}/game`);
+        this._socket = socketService.gameSocket;
         this._shouldInvertPositions = false;
 
         this.socket.on("connect", () => {
@@ -36,45 +55,83 @@ export class GameService extends EventEmitter {
         GameService._instance = this;
     }
 
+    /**
+     * Gets the socket connection for the game.
+     *
+     * @returns {Socket} The socket instance.
+     */
     get socket() {
         if (!this._socket.connected)
             this._socket.connect();
         return this._socket;
     }
 
-    reset() {
-        this._game = null;
-        this._context = null;
-        this._socket.disconnect();
-    }
-
+    /**
+     * Sets the rendering context for the game.
+     *
+     * @param {CanvasRenderingContext2D} context - The rendering context.
+     */
     set context(context) {
         this._context = context;
     }
 
-    handleRouteChange() {
-        this.reset();
-    }
-
-    isGameCreated() {
-        return this.game?.id != null;
-    }
-
+    /**
+     * Gets the current game instance.
+     *
+     * @returns {Game} The current game.
+     */
     get game() {
         return this._game;
     }
 
+    /**
+     * Sets the current game instance.
+     *
+     * @param {Game} newGame - The new game instance.
+     */
     set game(newGame) {
         this._game = newGame;
     }
 
+    /**
+     * Retrieves the singleton instance of GameService.
+     *
+     * @returns {GameService} The singleton instance.
+     */
     static getInstance() {
-        if (!GameService._instance) {
+        if (!GameService._instance)
             GameService._instance = new GameService();
-        }
+
         return GameService._instance;
     }
 
+    /**
+     * Resets the game state, clearing the current game and context.
+     */
+    reset() {
+        this._game = null;
+        this._context = null;
+    }
+
+    /**
+     * Handles route changes by resetting the game state.
+     */
+    handleRouteChange() {
+        this.reset();
+    }
+
+    /**
+     * Checks if a game has been created and initialized.
+     *
+     * @returns {boolean} True if the game is created, false otherwise.
+     */
+    isGameCreated() {
+        return this.game?.id != null;
+    }
+
+    /**
+     * Sets up event listeners for game updates received from the server.
+     */
     setupListeners() {
         this.socket.on("refreshStatus", (receivedData) => {
             const {status, data} = receivedData;
@@ -105,17 +162,31 @@ export class GameService extends EventEmitter {
         this.errorListener();
     }
 
-    handleGameCreated(data) {
-        this.game.id = data.id;
+    /**
+     * Handles the game creation event.
+     *
+     * @param {Object} data - The game creation data received from the server.
+     * @param {string} data.id - The unique identifier for the newly created game.
+     * @param {Object} data.players - An object containing the players participating in the game, keyed by their IDs.
+     */
+    handleGameCreated({id, players}) {
+        this.game.id = id;
 
-        this._shouldInvertPositions = Object.keys(data.players)[0] !== UserService.getInstance().user._id;
+        this._shouldInvertPositions = Object.keys(players)[0] !== userService.user._id;
         this.game.players = this._shouldInvertPositions
-            ? Object.fromEntries(Object.entries(data.players).reverse())
-            : data.players;
+            ? Object.fromEntries(Object.entries(players).reverse())
+            : players;
 
         this.emit(GameStatus.CREATED);
     }
 
+    /**
+     * Inverts a player's position on the board (e.g., for multiplayer games).
+     *
+     * @private
+     * @param {Object} position - The player's current position.
+     * @returns {Object} The inverted position.
+     */
     _invertPosition(position) {
         return {
             _row: position._row,
@@ -123,6 +194,11 @@ export class GameService extends EventEmitter {
         };
     }
 
+    /**
+     * Handles updates to player positions on the board.
+     *
+     * @param {Object} newPositions - The new positions of the players.
+     */
     handlePositionsUpdated(newPositions) {
         if (!this._shouldInvertPositions) {
             this.game.refreshBoard(newPositions, this._context);
@@ -139,12 +215,20 @@ export class GameService extends EventEmitter {
         this.game.refreshBoard(updatedPositions, this._context);
     }
 
+    /**
+     * Handles the end of a round.
+     *
+     * @param {Object} data - The round end data.
+     */
     handleRoundEnd(data) {
         this.emit(GameStatus.ROUND_END, data);
         this.game.resetBoard(this._context);
         this.game.playersPositions = [];
     }
 
+    /**
+     * Sets up an error listener for socket errors.
+     */
     errorListener() {
         this.socket.on("error", () => {
             alert("An error has occurred please try again");
@@ -152,6 +236,9 @@ export class GameService extends EventEmitter {
         });
     }
 
+    /**
+     * Handles the end of the game.
+     */
     handleGameEnd() {
         this._game = null;
         this._context = null;
@@ -160,17 +247,22 @@ export class GameService extends EventEmitter {
         this.emit(GameStatus.GAME_END);
     }
 
+    /**
+     * Starts a new game with the specified game type.
+     *
+     * @param {Number} gameType - The type of game to start.
+     */
     startGame(gameType) {
         if (this.game?.id)
             return;
 
-        const player = new LocalPlayer(UserService.getInstance().user._id, UserService.getInstance().user.name, UserService.getInstance().user.parameters.keysPlayers[0]);
+        const player = new LocalPlayer(userService.user._id, userService.user.name, userService.user.parameters.keysPlayers[0]);
 
         const players = [player];
         if (gameType === GameType.LOCAL) {
             const guest = new LocalPlayer("guest",
-                (UserService.getInstance().isConnected()) ? "Guest" : "Guest 1",
-                UserService.getInstance().user.parameters.keysPlayers[1]);
+                (userService.isConnected()) ? "Guest" : "Guest 1",
+                userService.user.parameters.keysPlayers[1]);
             players.push(guest);
         }
 
@@ -182,11 +274,21 @@ export class GameService extends EventEmitter {
         });
     }
 
+    /**
+     * Draws the current game state on the canvas.
+     */
     draw() {
         if (this.game)
             this.game.draw(this._context);
     }
 
+    /**
+     * Inverts a move
+     *
+     * @private
+     * @param {string} move - The move to invert.
+     * @returns {string} The inverted move.
+     */
     _invertMove(move) {
         const movements = Object.values(MovementTypes);
         const index = movements.indexOf(move);
@@ -197,6 +299,12 @@ export class GameService extends EventEmitter {
         return movements[mirroredIndex];
     }
 
+    /**
+     * Sends the player's next move to the server.
+     *
+     * @param {string} playerId - The ID of the player making the move.
+     * @param {string} move - The move to make.
+     */
     nextMove(playerId, move) {
         if (this._shouldInvertPositions)
             move = this._invertMove(move);
@@ -204,3 +312,5 @@ export class GameService extends EventEmitter {
         this.socket.emit("nextMove", {gameId: this.game.id, playerId, move});
     }
 }
+
+export const gameService = GameService.getInstance();
