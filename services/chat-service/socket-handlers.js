b2bcd55cddbfc2d2d9ba53c90970a70a60ccf06d
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-03-24 12:30:13 +0100

    docs(#76): Forgotten file

--- services/chat-service/socket-handlers.js
+++ services/chat-service/socket-handlers.js
@@ -4,8 +4,24 @@ const {DATABASE_ERRORS} = require("./utils");
 module.exports = (io) => {
     io.on('connection', (gatewaySocket) => {
         console.log("Socket connected", gatewaySocket.id);
+
+        // Connect the socket to a room using the user ID passed in the auth part to make it accessible by userId.
         gatewaySocket.join(gatewaySocket.handshake.auth.userId);
 
+        /**
+         * Handles marking messages as read in a conversation.
+         *
+         * If successful, it updates the status of the messages. If an error occurs,
+         * it emits an "error" event.
+         *
+         * @async
+         * @function
+         * @param {Array<string>} messageIds - The IDs of the messages to mark as read.
+         * @param {string} conversationId - The ID of the conversation where the messages belong.
+         * @param {string} userId - The ID of the user who is marking the messages as read.
+         * @listens messagesRead
+         * @event error - Emitted if an error occurs while marking the messages as read.
+         */
         gatewaySocket.on("messagesRead", async (messageIds, conversationId, userId) => {
             try {
                 await markMessagesAsRead(messageIds, userId);
@@ -15,10 +31,34 @@ module.exports = (io) => {
             }
         });
 
+        /**
+         * Handles the joining of one or more conversation rooms by the socket.
+         *
+         * The socket joins the rooms corresponding to the provided conversation IDs.
+         *
+         * @function
+         * @param {Array<string>} conversationIds - The IDs of the conversations to join.
+         * @listens joinConversations
+         * @event joinConversations - Emitted when a socket joins one or more conversation rooms.
+         */
         gatewaySocket.on("joinConversations", (conversationIds) => {
             gatewaySocket.join(conversationIds);
         });
 
+        /**
+         * Handles the deletion of a message in a conversation.
+         *
+         * If the message is successfully deleted, it emits a "deleteMessage" event
+         * to all participants in the conversation. If an error occurs, it emits an "error" event.
+         *
+         * @async
+         * @function
+         * @param {string} messageId - The ID of the message to delete.
+         * @param {string} userId - The ID of the user requesting the deletion.
+         * @listens deleteMessage
+         * @event deleteMessage - Emitted when a message is deleted in a conversation.
+         * @event error - Emitted if an error occurs during message deletion.
+         */
         gatewaySocket.on("deleteMessage", async (messageId, userId) => {
             try {
                 const conversationId = await deleteMessageWithOwner(messageId, userId);
@@ -29,6 +69,24 @@ module.exports = (io) => {
             }
         });
 
+        /**
+         * Handles the sending of a message in a conversation.
+         *
+         * If any required fields (content, conversationId, or senderId) are missing,
+         * it emits an "error" event. Otherwise, it saves the message, adds a timestamp,
+         * and emits a "message" event to all participants in the conversation.
+         * If a callback is provided, it is called with the new message.
+         *
+         * @async
+         * @function
+         * @param {string} content - The content of the message being sent.
+         * @param {string} conversationId - The ID of the conversation where the message is sent.
+         * @param {string} senderId - The ID of the user sending the message.
+         * @param {function} [callback] - An optional callback to handle the new message.
+         * @listens message
+         * @event message - Emitted when a new message is sent to the conversation.
+         * @event error - Emitted if required fields are missing or message saving fails.
+         */
         gatewaySocket.on("message", async (content, conversationId, senderId, callback) => {
             try {
                 if (!content || !conversationId || !senderId) {
@@ -56,6 +114,22 @@ module.exports = (io) => {
             }
         });
 
+        /**
+         * Handles the creation of a conversation between two users.
+         *
+         * If a conversation between the users already exists, it emits a "conversationExists" event
+         * with the existing conversation ID. Otherwise, it creates a new conversation, updates the
+         * participant lists, and emits a "newConversation" event to both users.
+         *
+         * @async
+         * @function
+         * @param {string} userId - The ID of the user initiating the conversation.
+         * @param {string} friendsId - The ID of the friend to start the conversation with.
+         * @listens createConversation
+         * @event conversationExists - Emitted if the conversation already exists.
+         * @event newConversation - Emitted when a new conversation is created.
+         * @event error - Emitted if an error occurs during conversation creation.
+         */
         gatewaySocket.on("createConversation", async (userId, friendsId) => {
             try {
                 const conversationId = await getConversationIdIfExists(userId, friendsId);
@@ -85,7 +159,6 @@ module.exports = (io) => {
                 else
                     gatewaySocket.emit("error", {message: error.message});
             }
-
         });
     });
 };
