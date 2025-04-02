6cc24c6c6b1a6f51e1f86f319453b56acc08e17b
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-04-03 00:48:11 +0200

    feat(#92): Add a `NotificationsStore` for storing notifications on the client side, along with some events and function useful for the front end in general

--- services/files/front/services/notifications-service.js
+++ services/files/front/services/notifications-service.js
@@ -1,14 +1,41 @@
 import {socketService} from "./socket-service.js";
 import {USER_EVENTS, userService} from "./user-service.js";
+import {notificationStore} from "../js/NotificationsStore.js";
+import {EventEmitter} from "../js/EventEmitter.js";
 
-class NotificationsService {
+/**
+ *  Defines the notifications events
+ *
+ * @constant {Object}
+ */
+export const NOTIFICATIONS_EVENTS = Object.freeze({
+    NOTIFICATIONS_UPDATED: "NOTIFICATIONS_UPDATED",
+    NOTIFICATIONS_ADDED: "NOTIFICATIONS_ADDED"
+});
+
+/**
+ * Defines the type of notifications
+ *
+ * @constant {Object}
+ */
+export const NOTIFICATIONS_TYPE = Object.freeze({
+    NEW_MESSAGE: "newMessage",
+    FRIEND_REQUEST: "friendRequest",
+    FRIEND_DELETION: "friendDeletion",
+    FRIEND_ACCEPT: "friendAccept",
+    FRIENDLY_GAME: "friendlyGame"
+});
+
+class NotificationsService extends EventEmitter {
 
     constructor() {
+        super();
         if (NotificationsService._instance)
             return NotificationsService._instance;
 
         this._apiUrl = "api/notifications";
         NotificationsService._instance = this;
+        this._notificationsStore = notificationStore;
     }
 
     /**
@@ -46,6 +73,16 @@ class NotificationsService {
      */
     initSocket() {
         this._socket = socketService.connectNotificationsSocket();
+        this._setupNotificationsSocketListeners();
+    }
+
+    /**
+     * Retrieves the list of notifications from the NotificationsStore.
+     *
+     * @return {NotificationsStore._notifications} The notifications stored in the NotificationsStore.
+     */
+    getNotifications() {
+        return this._notificationsStore.notifications;
     }
 
     /**
@@ -57,7 +94,6 @@ class NotificationsService {
      * @returns {Promise<void>} Resolves when the initialization is complete.
      */
     async init() {
-        console.log("test");
         userService.on(USER_EVENTS.CONNECTION, () => {
             this.initSocket();
         });
@@ -65,6 +101,48 @@ class NotificationsService {
         if (userService.isConnected()) {
             this.initSocket();
         }
+    }
+
+    /**
+     * Set all the field isRead to true for all the notifications and send an event to the back-end
+     */
+    setAllRead() {
+        this._notificationsStore.setAllRead();
+        this._socket.emit("notificationsRead");
+        this.emit(NOTIFICATIONS_EVENTS.NOTIFICATIONS_UPDATED);
+    }
+
+    /**
+     * Sets up the socket event listeners for notification addition.
+     *
+     * This function listens for two socket events:
+     * 1. **"new-notification"**: When a notification is sent to the user, the event handler add the notification to
+     *    the notifications list in the NotificationsStore.
+     * 2. **"notifications"**: When the user is connected receive the notification to update the notifications list in the NotificationsStore.
+     */
+    _setupNotificationsSocketListeners() {
+        this.socket.on("connect", () => {
+            if (!this._notificationsStore.fetch) {
+                this.socket.emit("getNotifications");
+            }
+        });
+
+        this.socket.on("getNotifications", (data) => {
+            if (!this._notificationsStore.fetch) {
+                this._notificationsStore.notifications = data.reduce((acc, notification) => {
+                    acc.set(notification._id, notification);
+                    return acc;
+                }, new Map());
+            }
+            this.emit(NOTIFICATIONS_EVENTS.NOTIFICATIONS_UPDATED);
+        });
+
+        this.socket.on("new-notification", (data) => {
+            this._notificationsStore.addNotification(data.notification);
+            this.emit(NOTIFICATIONS_EVENTS.NOTIFICATIONS_UPDATED);
+        });
+
+        this.socket.on("disconnect", () => this._notificationsStore.clear());
     }
 }
 
