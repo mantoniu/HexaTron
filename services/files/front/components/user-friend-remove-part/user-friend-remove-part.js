bd99ac18af6c6fef52fe696d46e105d7ca7c3adb
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-04-06 18:26:22 +0200

    feat(#97): Addition of a challenge button

--- services/files/front/components/user-friend-remove-part/user-friend-remove-part.js
+++ services/files/front/components/user-friend-remove-part/user-friend-remove-part.js
@@ -4,7 +4,8 @@ import {chatService} from "../../services/chat-service.js";
 export class UserFriendRemovePart extends Component {
     static SELECTORS = {
         MESSAGE: "sendMessage",
-        DELETE_FRIEND: "deleteFriend"
+        DELETE_FRIEND: "deleteFriend",
+        CHALLENGE: "challenge"
     };
 
     constructor() {
@@ -15,14 +16,19 @@ export class UserFriendRemovePart extends Component {
     async connectedCallback() {
         await super.connectedCallback();
         this._elements = this.initializeElements(UserFriendRemovePart.SELECTORS);
+
         if (JSON.parse(this.getAttribute("deletion-desactivate"))) {
             this.style.marginTop = "0px";
             this._elements.DELETE_FRIEND.style.display = "none";
         }
+
+        this._friendId = this.getAttribute("friendId");
+
         this.setupEventListeners();
     }
 
     setupEventListeners() {
+        this.addAutoCleanListener(this._elements.CHALLENGE, "click", () => this._launchFriendlyGame());
         this.addAutoCleanListener(this._elements.DELETE_FRIEND, "click", (click) => this.handleFriendDeletion(click));
         this.addAutoCleanListener(this._elements.MESSAGE, "click", (click) => this.sendMessage(click));
     }
@@ -39,6 +45,20 @@ export class UserFriendRemovePart extends Component {
 
     sendMessage(click) {
         click.stopPropagation();
-        chatService.createConversation(this.getAttribute("friendId"));
+        chatService.createConversation(this._friendId);
+    }
+
+    _launchFriendlyGame() {
+        this.dispatchEvent(new CustomEvent("closeDrawer", {
+            composed: true,
+            bubbles: true
+        }));
+
+        window.dispatchEvent(new CustomEvent("navigate", {
+            detail: {
+                route: `/friendly`,
+                params: {friendId: this._friendId}
+            }
+        }));
     }
 }
