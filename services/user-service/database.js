cde487426d6762025fa289c64b4f7f36419f6b36
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-04-07 22:39:18 +0200

    fix(#103): FIx problem of the refresh token

--- services/user-service/database.js
+++ services/user-service/database.js
@@ -240,7 +240,7 @@ function generateToken(userId, access) {
         return jwt.sign(
             {userId},
             access ? process.env.ACCESS_TOKEN_SECRET : process.env.REFRESH_TOKEN_SECRET,
-            {expiresIn: access ? "15m" : "7d"}
+            {expiresIn: access ? "1m" : "2m"}
         );
     } catch (error) {
         throw new Error(DATABASE_ERRORS.TOKEN_GENERATION_FAILED);
