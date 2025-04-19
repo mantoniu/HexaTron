cf5adec48942beee0210aa6863a4e8cbdf9e0264
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-04-19 11:45:43 +0200

    feat(#123): Add a script to retrieve the API URL from the Java part of the app using a plugin

--- services/files/front/js/config.js
+++ services/files/front/js/config.js
@@ -1,3 +1,16 @@
+let url = "https://hexatron.ps8.pns.academy";
+
+export const mobile = Capacitor.isNativePlatform();
+
+if (mobile) {
+    const {EnvPlugin} = Capacitor.Plugins;
+    if (EnvPlugin) {
+        const {apiUrl} = await EnvPlugin.getApiUrl();
+        url = apiUrl;
+    }
+}
+
+
 export const API_HOST = Capacitor.getPlatform() === "web"
     ? window.location.origin
-    : "https://hexatron.ps8.pns.academy";
+    : url;
