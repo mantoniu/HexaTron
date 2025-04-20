c387069fdc1fe0d4c1930ab75b59ae84a1a8738b
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-04-20 10:53:08 +0200

    feat(#115): Add a component to present game types

--- services/files/front/components/game-types-presentation-component/game-types-presentation-component.js
+++ services/files/front/components/game-types-presentation-component/game-types-presentation-component.js
@@ -1,7 +1,5 @@
 import {ListenerComponent} from "../component/listener-component.js";
 
-export const gameTypes = ["local", "ai", "ranked", "friendly"];
-
 export class GameTypesPresentationComponent extends ListenerComponent {
     constructor() {
         super();
@@ -9,5 +7,18 @@ export class GameTypesPresentationComponent extends ListenerComponent {
 
     async connectedCallback() {
         await super.connectedCallback();
+
+        this._initialize();
+    }
+
+    _initialize() {
+        const shadowRoot = this.shadowRoot;
+        const title = shadowRoot.getElementById("title");
+        const icon = shadowRoot.getElementById("icon-game-type");
+
+        if (title)
+            title.textContent = this.getAttribute("title") || "";
+        if (icon)
+            icon.src = this.getAttribute("icon") || "";
     }
 }
