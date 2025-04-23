c843697e5750e81f3fdde4430f70c8a7609f883f
LubratJilian <jilian.lubrat@etu.unice.fr>
2025-04-23 22:03:04 +0200

    fix(#147): Remove warning in the code

--- services/files/front/components/drawer-menu/drawer-menu.js
+++ services/files/front/components/drawer-menu/drawer-menu.js
@@ -91,13 +91,13 @@ export class DrawerMenu extends ListenerComponent {
                         notificationService.sendUpdateEvent();
                     break;
                 case DRAWER_CONTENT.FRIENDS:
-                    if (!NOTIFICATIONS_TYPE.NEW_MESSAGE && !NOTIFICATIONS_TYPE.GAME_INVITATION) {
+                    if (!NOTIFICATIONS_TYPE.NEW_MESSAGE && !NOTIFICATIONS_TYPE.GAME_INVITATION)
                         notificationService.removeFriendsNotifications();
-                        break;
-                    }
+                    else
+                        notificationService.sendUpdateEvent();
+                    break;
                 default:
                     notificationService.sendUpdateEvent();
-                    break;
             }
         });
     }
